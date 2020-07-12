c495578054c57410b3e6f530848c3d19f67b69e6
shusuke0812 <shu.ota0812@gmail.com>
2020-07-12 22:50:56 +0900

    [add]ボタンタップ実装
    ・ViewController追加
    ・タップした際、コンソールに「タップされた」を出力させるようにした

---
+++ UICatalogForiOS/ViewController/FloatingButtonViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FloatingButtonViewController.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/7/12.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class FloatingButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func buttonTap(_ sender: Any) {
+        print("DEBUG：　ボタンがタップされました")
+    }
+}
