b328e80a9409f377402c5f3578bcbbac33e173f0
shusuke0812 <shu.ota0812@gmail.com>
2020-07-28 19:01:31 +0900

    [add]アコーディオンメニューの処理を実装

--- UICatalogForiOS/ViewController/AccordionMenu/AccordionMenuViewController.swift
+++ UICatalogForiOS/ViewController/AccordionMenu/AccordionMenuViewController.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class AccordionMenuViewController: UIViewController {
 
+    var showSaleTimeDetail = true
+    
+    @IBOutlet weak var saleTimeDetail: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension AccordionMenuViewController {
+    @IBAction func showSaleTimeDetailButton(_ sender: Any) {
+        showSaleTimeDetail = !showSaleTimeDetail
+        UIView.animate(withDuration: 0.3) {
+            self.saleTimeDetail.isHidden = !self.showSaleTimeDetail
+        }
     }
-    */
-
 }
