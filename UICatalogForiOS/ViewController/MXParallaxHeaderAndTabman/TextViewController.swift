0cbe5dba0cba54f0cc7a7f426a97e20025493889
shusuke0812 <shu.ota0812@gmail.com>
2020-08-01 02:13:33 +0900

    [add]前画面から受け取る値を格納するメンバを定義

--- UICatalogForiOS/ViewController/MXParallaxHeaderAndTabman/TextViewController.swift
+++ UICatalogForiOS/ViewController/MXParallaxHeaderAndTabman/TextViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class TextViewController: UIViewController {
 
+    
+    @IBOutlet weak var getValueLabel: UILabel!
+    var getPreviousPageValue: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getValueLabel.text = getPreviousPageValue
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
