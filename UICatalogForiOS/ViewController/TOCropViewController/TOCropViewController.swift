60d42d02b4d132d465b8125ea473605da2ca00b4
shusuke0812 <shu.ota0812@gmail.com>
2020-09-12 20:20:39 +0900

    [add]imagePickerと画像トリム関数を追加

--- UICatalogForiOS/ViewController/TOCropViewController/TOCropViewController.swift
+++ UICatalogForiOS/ViewController/TOCropViewController/TOCropViewController.swift
@@ -7,10 +7,36 @@
 //
 
 import UIKit
+import CropViewController
 
 class TOCropViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func tapImageView(_ sender: Any) {
+    }
+}
+
+extension TOCropViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate,  CropViewControllerDelegate {
+    func setImagePicker() {
+        let picker: UIImagePickerController = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let originalImage: UIImage = (info[UIImagePickerController.InfoKey.originalImage] as? UIImage) else { return }
+        
+        let cropVC: CropViewController = CropViewController(croppingStyle: .circular, image: originalImage)
+        cropVC.delegate = self
+        picker.dismiss(animated: true, completion: {
+            self.present(cropVC, animated: true, completion: nil)
+        })
+    }
+    
+    func cropViewController(_ cropViewController: CropViewController, didCropToCircularImage image: UIImage, withRect cropRect: CGRect, angle: Int) {
+        print("test")
     }
 }
