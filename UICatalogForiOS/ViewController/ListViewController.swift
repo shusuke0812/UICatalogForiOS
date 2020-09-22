d36bd1fe78b4cb8afb9439754c681b8392ba688e
shusuke0812 <shu.ota0812@gmail.com>
2020-09-22 23:13:45 +0900

    [add]ライブラリリスト用のviewControllerを設定

--- UICatalogForiOS/ViewController/ListViewController.swift
+++ UICatalogForiOS/ViewController/ListViewController.swift
@@ -11,9 +11,46 @@ import UIKit
 class ListViewController: UIViewController {
     /// BaseView
     private var baseView: ListBaseView { return self.view as! ListBaseView}
+    /// ViewModel
+    private var viewModel: ListViewModel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = ListViewModel()
+        self.setBaseView()
+    }
+}
+
+extension ListViewController: UITableViewDelegate {
+    private func setBaseView() {
+        self.baseView.tableView.tableFooterView = UIView()
+        self.baseView.tableView.delegate = self
+        self.baseView.tableView.dataSource = self.viewModel
+    }
+}
+
+extension ListViewController {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let sectionType: ListSectionType = self.viewModel.sections[indexPath.section]
+        switch  sectionType {
+        case .ui:
+            return 50
+        case .image:
+            return 50
+        }
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sectionRowType: ListRowType = self.viewModel.sections[indexPath.section].rows[indexPath.row]
+        switch sectionRowType {
+        case .mxparallax:
+            print("DEBUG： mxparallaxがタップされました")
+        case .xlpager:
+            print("DEBUG： xlpagerがタップされました")
+        case .tocrop:
+            print("DEBUG： tocropがタップされました")
+        case .imageViewer:
+            print("DEBUG： imageViewerがタップされました")
+        }
     }
 }
