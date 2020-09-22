8291c43e6f3967f6bc491758d70e037fe003a6a9
shusuke0812 <shu.ota0812@gmail.com>
2020-09-22 22:41:37 +0900

    [add]ライブラリ一覧のbaseViewを追加

--- UICatalogForiOS/View/BaseView/ListBaseView.swift
+++ UICatalogForiOS/View/BaseView/ListBaseView.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ListBaseView: UIView {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.registerTableViewCell()
+    }
+}
+
+extension ListBaseView {
+    private func registerTableViewCell() {
+        let nib = UINib(nibName: "ListViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "ListViewCell")
+    }
+}
