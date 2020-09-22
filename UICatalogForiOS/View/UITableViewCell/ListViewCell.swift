dc8817512b0c96a94c72f46e0f6a5bd0e148fd2d
shusuke0812 <shu.ota0812@gmail.com>
2020-09-22 22:33:05 +0900

    [add]ライブラリまとめ用のtableViewファイルを追加

---
+++ UICatalogForiOS/View/UITableViewCell/ListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewCell.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/9/22.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
