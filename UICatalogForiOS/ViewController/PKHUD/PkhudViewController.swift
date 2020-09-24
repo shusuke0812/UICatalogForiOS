51d6705c40d29cb30503de992ee3ca560dd0e3c5
shusuke0812 <shu.ota0812@gmail.com>
2020-09-25 01:27:22 +0900

    [add]PKHUD用のviewControllerとstoryboardファイルを追加

---
+++ UICatalogForiOS/ViewController/PKHUD/PkhudViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PkhudViewController.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/9/25.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+import PKHUD
+
+class PkhudViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
