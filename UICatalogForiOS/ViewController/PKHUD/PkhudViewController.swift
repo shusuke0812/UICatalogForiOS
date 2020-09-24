e3b940d1e8d99a2c3122d9d2169a2b19b5f530d0
shusuke0812 <shu.ota0812@gmail.com>
2020-09-25 08:54:15 +0900

    [add]HUD処理を追加

--- UICatalogForiOS/ViewController/PKHUD/PkhudViewController.swift
+++ UICatalogForiOS/ViewController/PKHUD/PkhudViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2020 shusuke. All rights reserved.
 //
 
+/*
+ * このクラスで実装されている主な機能
+ *   - ボタンをタップするとロード中であるHUDが表示され、指定時間後に閉じる機能
+ */
+
 import UIKit
 import PKHUD
 
@@ -16,5 +21,25 @@ class PkhudViewController: UIViewController {
     }
     // MARK: - Action Method
     @IBAction func startHudButton(_ sender: Any) {
+        #if false
+        // HUD表示
+        HUD.show(.progress)
+        // 指定時間後にHUDを閉じる
+        HUD.flash(.progress, delay: 5) { _ in
+            print("DEBUG： HUDが閉じられました")
+        }
+        #endif
+        
+        #if true
+        // HUD表示
+        HUD.show(.progress)
+        // 5秒後の遅延処理
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            // HUDを閉じる
+            HUD.hide() { _ in
+                print("DEBUG： HUDが閉じられました")
+            }
+        }
+        #endif
     }
 }
