1176316e05871aa133ece6db3001d75e5487914b
shusuke0812 <shu.ota0812@gmail.com>
2020-11-05 13:34:22 +0900

    [add]ImageViewerOnTableViewのモーダル画面をスワイプで閉じれないようにした

---
+++ UICatalogForiOS/View/BaseView/ImageViewerOnTableViewBaseView.swift
@@ -0,0 +1,23 @@
+//
+//  ImageViewerOnTableViewBaseView.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/11/5.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewerOnTableViewBase: UIView {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewWithCollectionView: UITableView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
+// MARK: - Initialized Method
+extension ImageViewerOnTableViewController {
+    
+}
--- UICatalogForiOS/ViewController/ImageViewer/ImageViewerOnTableViewController.swift
+++ UICatalogForiOS/ViewController/ImageViewer/ImageViewerOnTableViewController.swift
@@ -12,5 +12,17 @@ class ImageViewerOnTableViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUIControl()
+    }
+    // MARK: - Action Method
+    @IBAction func didTapCloseButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+// MARK: - Initialized Method
+extension ImageViewerOnTableViewController {
+    private func setUIControl() {
+        // モーダル画面をスワイプで閉じれないようにする
+        self.isModalInPresentation = true
     }
 }
