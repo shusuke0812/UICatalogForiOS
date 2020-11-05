b940cc5adff53547fa2b7214ad9d34a90a9182ca
shusuke0812 <shu.ota0812@gmail.com>
2020-11-05 15:11:49 +0900

    [add]ImageViewerOnTableViewModelを追加

---
+++ UICatalogForiOS/ViewModel/ImageViewerOnTableViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  ImageViewerOnTableViewModel.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2020/11/5.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewerOnTableViewModel: NSObject {
+}
+
+// MARK: - TableView DataSource Method
+extension ImageViewerOnTableViewModel: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("")
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = self.checkTableViewKind(tableView)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        return cell
+    }
+}
+extension ImageViewerOnTableViewModel {
+    func checkTableViewKind(_ tableView: UITableView) -> String {
+        var cellIdentifier: String
+        switch tableView.tag {
+        case TableViewTag.normal.rawValue:
+            cellIdentifier = ""
+            return cellIdentifier
+        case TableViewTag.withCollectionView.rawValue:
+            cellIdentifier = ""
+            return cellIdentifier
+        default:
+            return ""
+        }
+    }
+}
