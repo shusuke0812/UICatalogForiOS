e2055d4d36c2377f496ce0f79ed8afd3243037eb
shusuke0812 <shu.ota0812@gmail.com>
2020-11-05 13:39:04 +0900

    [fix]クラス名を修正

--- UICatalogForiOS/View/BaseView/ImageViewerOnTableViewBaseView.swift
+++ UICatalogForiOS/View/BaseView/ImageViewerOnTableViewBaseView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ImageViewerOnTableViewBase: UIView {
+class ImageViewerOnTableViewBaseView: UIView {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var tableViewWithCollectionView: UITableView!
