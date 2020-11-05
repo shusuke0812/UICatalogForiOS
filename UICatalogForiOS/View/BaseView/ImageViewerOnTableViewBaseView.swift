7b36a09395b36b23e7def135073343a5a67e1e2c
shusuke0812 <shu.ota0812@gmail.com>
2020-11-05 16:16:56 +0900

    [add]セル登録のコードを追加

--- UICatalogForiOS/View/BaseView/ImageViewerOnTableViewBaseView.swift
+++ UICatalogForiOS/View/BaseView/ImageViewerOnTableViewBaseView.swift
@@ -27,7 +27,13 @@ class ImageViewerOnTableViewBaseView: UIView {
 // MARK: - Initialized Method
 extension ImageViewerOnTableViewBaseView {
     private func setTableView() {
+        // タグの登録
         self.tableView.tag = TableViewTag.normal.rawValue
         self.tableViewWithCollectionView.tag = TableViewTag.withCollectionView.rawValue
+        // セルの登録
+        let NormalNib = UINib(nibName: Static.CellId.normal, bundle: nil)
+        self.tableView.register(NormalNib, forCellReuseIdentifier: Static.CellId.normal)
+        let CollectionNib = UINib(nibName: Static.CellId.withCollectionView, bundle: nil)
+        self.tableViewWithCollectionView.register(CollectionNib, forCellReuseIdentifier: Static.CellId.withCollectionView)
     }
 }
