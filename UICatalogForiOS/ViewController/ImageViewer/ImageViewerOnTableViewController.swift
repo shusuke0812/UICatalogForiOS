2deed38415ad34870de01e16e7daa45f6bb057a3
shusuke0812 <shu.ota0812@gmail.com>
2020-11-05 15:13:40 +0900

    [fix]ImageViewerOnTableViewControllerにBaseViewとViewModelを接続し、delegate登録処理を追加

--- UICatalogForiOS/ViewController/ImageViewer/ImageViewerOnTableViewController.swift
+++ UICatalogForiOS/ViewController/ImageViewer/ImageViewerOnTableViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ImageViewerOnTableViewController: UIViewController {
+    
+    /// BaseView
+    private var baseView: ImageViewerOnTableViewBaseView { self.view as! ImageViewerOnTableViewBaseView}
+    /// ViewModel
+    private var viewModel: ImageViewerOnTableViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setUIControl()
+        self.viewModel = ImageViewerOnTableViewModel()
     }
     // MARK: - Action Method
     @IBAction func didTapCloseButton(_ sender: Any) {
@@ -24,5 +30,14 @@ extension ImageViewerOnTableViewController {
     private func setUIControl() {
         // モーダル画面をスワイプで閉じれないようにする
         self.isModalInPresentation = true
+    }
+}
+// MARK: - TableView Delegate Method
+extension ImageViewerOnTableViewController: UITableViewDelegate {
+    private func setTableView() {
+        self.baseView.tableView.delegate = self
+        self.baseView.tableView.dataSource = self.viewModel
+        self.baseView.tableViewWithCollectionView.delegate = self
+        self.baseView.tableViewWithCollectionView.dataSource = self.viewModel
     }
 }
