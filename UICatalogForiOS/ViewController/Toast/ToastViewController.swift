bb638e53edbbac395e335138f34226731ceef9d4
shusuke0812 <shu.ota0812@gmail.com>
2021-01-16 09:06:48 +0900

    [add]Toast用のViewControllerとStoryboardファイルを追加

---
+++ UICatalogForiOS/ViewController/Toast/ToastViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ToastViewController.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2021/1/16.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ToastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
