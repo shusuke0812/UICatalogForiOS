4c46f1bedd5762dc613f562d975fa6bd9fa854f8
shusuke0812 <shu.ota0812@gmail.com>
2021-01-16 14:38:15 +0900

    [add]トーストで表示させるCustomViewを作成

---
+++ UICatalogForiOS/View/UIView/ToastView.swift
@@ -0,0 +1,29 @@
+//
+//  ToastView.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2021/1/16.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ToastView: XibView {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initUI()
+    }
+}
+// MARK: - Initialized Method
+extension ToastView {
+    func initUI() {
+        // ラベルの設定
+        self.label.text = "toast image"
+        self.label.tintColor = .white
+        // 画像の設定
+        self.imageView.image = UIImage(named: "image_sample")
+    }
+}
