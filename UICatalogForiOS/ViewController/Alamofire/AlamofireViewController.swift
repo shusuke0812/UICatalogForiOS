8efcf044492264ecf98bca0288f23c71ea442422
shusuke0812 <shu.ota0812@gmail.com>
2021-06-25 00:28:28 +0900

    [add]Alamofire用のVCにViewModel/Viewの設定をした

--- UICatalogForiOS/ViewController/Alamofire/AlamofireViewController.swift
+++ UICatalogForiOS/ViewController/Alamofire/AlamofireViewController.swift
@@ -9,8 +9,49 @@
 import UIKit
 
 class AlamofireViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    /// ViewModel
+    private var viewModel: AlamofireViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.viewModel = AlamofireViewModel(gitHubRepositorySearchRepository: GitHubRepositorySearchRepository())
+        self.setDelegate()
+    }
+}
+// MARK: - Init
+extension AlamofireViewController {
+    private func setDelegate() {
+        self.searchBar.delegate = self
+        self.tableView.delegate = self
+        self.tableView.dataSource = self.viewModel
+        self.viewModel.delegate = self
+    }
+}
+// MARK: - UISearchBar Delegate
+extension AlamofireViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchWord = searchBar.text else { return }
+        self.viewModel.getGitHubRepositorys(searchWord: searchWord)
+        self.searchBar.endEditing(true)
+    }
+}
+// MARK: - TableView Delegate
+extension AlamofireViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+// MARK: - ViewModel Delegate
+extension AlamofireViewController: AlamofireViewModeDelegate {
+    func didSuccessGetGitHubRepositorys() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    func didFailedGetGitHubRepositorys(errorMessaage: String) {
+        print("DEBUG: ", errorMessaage)
     }
 }
