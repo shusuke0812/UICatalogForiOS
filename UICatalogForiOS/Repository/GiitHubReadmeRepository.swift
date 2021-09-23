ffc9e93a5239a25852ffa537a8b176f180033f7f
shusuke0812 <shu.ota0812@gmail.com>
2021-09-23 23:58:20 +0900

    add: GitHubREADME取得用のRepositoryクラスを追加

---
+++ UICatalogForiOS/Repository/GiitHubReadmeRepository.swift
@@ -0,0 +1,36 @@
+//
+//  GiitHubReadmeRepository.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2021/9/23.
+//  Copyright © 2021 shusuke. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubReadmeRepositoryProtocol {
+    /// GitHubリポジトリのREADMEを取得する
+    /// - Parameters:
+    ///  - ownerName: リポジトリアカウント名
+    ///  - repositoryName: リポジトリ名
+    ///  - completion: README取得の成功、失敗ハンドル
+    func getRepositoryReadme(ownerName: String, repositoryName: String, completion: @escaping (Result<GitHubReadme, APIClientError>) -> Void)
+}
+
+class GitHubReadmeRepository: GitHubReadmeRepositoryProtocol {
+}
+
+// MARK: - API Method [GET]
+extension GitHubReadmeRepository {
+    func getRepositoryReadme(ownerName: String, repositoryName: String, completion: @escaping (Result<GitHubReadme, APIClientError>) -> Void) {
+        let request = GetReadmeRequest(ownerName: ownerName, repositoryName: repositoryName)
+        AlamofireAPIClient.shared.sendRequest(request) { result in
+            switch result {
+            case .success(let response):
+                completion(.success(resonse))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
