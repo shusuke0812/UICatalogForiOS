44164fc31ab5f99a3ac6643f71bdbd4c54653cea
shusuke0812 <shu.ota0812@gmail.com>
2022-10-08 23:52:06 +0900

    refactor: TableViewCellを生成するfactoryを設定

--- UICatalogForiOS/ViewModel/ListViewModel.swift
+++ UICatalogForiOS/ViewModel/ListViewModel.swift
@@ -46,12 +46,18 @@ enum ListRowType: String {
 }
 
 class ListViewModel: NSObject {
-    // tavbleViewのセクション
     var sections: [ListSectionType] = []
-    // セクション設定
+
     override init() {
         super.init()
         self.setSections()
+    }
+    
+    private func listViewCellFactory(sectionType: ListSectionType, tableView: UITableView, cellForRowAt indexPath: IndexPath) -> ListViewCell {
+        let cell: ListViewCell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
+        let rowType: ListRowType = sectionType.rows[indexPath.row]
+        cell.titleLabel.text = rowType.rawValue
+        return cell
     }
 }
 
@@ -70,28 +76,6 @@ extension ListViewModel: UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let sectionType: ListSectionType = self.sections[indexPath.section]
-        switch sectionType {
-        // TODO： セル生成のコードが重複しているのでうまくまとめる方法を考える
-        case .ui:
-            let cell: ListViewCell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
-            let rowType: ListRowType = sectionType.rows[indexPath.row]
-            cell.titleLabel.text = rowType.rawValue
-            return cell
-        case .image:
-            let cell: ListViewCell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
-            let rowType: ListRowType = sectionType.rows[indexPath.row]
-            cell.titleLabel.text = rowType.rawValue
-            return cell
-        case .interaction:
-            let cell: ListViewCell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
-            let rowType: ListRowType = sectionType.rows[indexPath.row]
-            cell.titleLabel.text = rowType.rawValue
-            return cell
-        case .networking:
-            let cell: ListViewCell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
-            let rowType: ListRowType = sectionType.rows[indexPath.row]
-            cell.titleLabel.text = rowType.rawValue
-            return cell
-        }
+        return listViewCellFactory(sectionType: sectionType, tableView: tableView, cellForRowAt: indexPath)
     }
 }
