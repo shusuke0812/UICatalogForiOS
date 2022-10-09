3417c333a67a8df5934ce4e7656b0a7b3f3bf5bc
shusuke0812 <shu.ota0812@gmail.com>
2022-10-09 11:23:21 +0900

    add: Hydra alwaysサンプル追加、Hydra実行ボタン・実行結果表示ボタンを追加

---
+++ UICatalogForiOS/Screen/Hydra/HydraError.swift
@@ -0,0 +1,13 @@
+//
+//  HydraError.swift
+//  UICatalogForiOS
+//
+//  Created by Shusuke Ota on 2022/10/9.
+//  Copyright © 2022 shusuke. All rights reserved.
+//
+
+import Foundation
+
+enum HydraError: Error {
+    case `default`
+}
--- UICatalogForiOS/Screen/Hydra/HydraViewController.swift
+++ UICatalogForiOS/Screen/Hydra/HydraViewController.swift
@@ -7,24 +7,62 @@
 //
 
 import UIKit
+import Hydra
 
 class HydraViewController: UIViewController {
-
+    @IBOutlet private weak var promiseRunButton: UIButton!
+    @IBOutlet private weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initialize()
+    }
+    // MARK: Action
+    @IBAction private func promiseRun(_ sender: UIButton) {
+        runAlways()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initialize() {
+        promiseRunButton.setTitle("Promise実行", for: .normal)
+        promiseRunButton.backgroundColor = .darkGray
+        promiseRunButton.tintColor = .white
+        promiseRunButton.layer.cornerRadius = 4
+        promiseRunButton.clipsToBounds = true
+        
+        resultLabel.text = "-"
+        resultLabel.textAlignment = .center
+        resultLabel.font = .systemFont(ofSize: 17, weight: .bold)
     }
-    */
+    private func configResultLabel(_ text: String) {
+        DispatchQueue.main.async {
+            sleep(1)
+            self.resultLabel.text = text
+        }
+    }
+}
 
+extension HydraViewController {
+    // MARK: always
+    private func alwaysSample() -> Promise<Void> {
+        return Promise<Void>(in: .background, { resolve, reject, _ in
+            if Int.random(in: 0..<2) == 0 {
+                resolve(())
+            } else {
+                reject(HydraError.default)
+            }
+        })
+    }
+    private func runAlways() {
+        alwaysSample().then { [weak self] _ in
+            debugPrint("then")
+            self?.configResultLabel("then")
+        }.catch { [weak self] error in
+            debugPrint("catch: \(error)")
+            self?.configResultLabel("catch: \(error)")
+        }.always { [weak self] in
+            // Promiseの結果が resole でも reject でも最後に必ず呼ばれる
+            debugPrint("always")
+            self?.configResultLabel("always")
+        }
+    }
 }
